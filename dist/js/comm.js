995dc48881c947c237d91861f2ca1cd1497bb15c
choi11223344 <choi890220@gmail.com>
2023-08-04 17:40:38 +0900

    230804-chej-nav3

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -19,22 +19,43 @@ searchInputEl.addEventListener("blur", function () {
   searchInputEl.setAttribute("placeholder", "");
 });
 
-$(function () {
-  // tabs
-  const tabEl = $(".tab-item");
-  const tabTit = tabEl.find(".tit");
+// $(function () {
+//   // tabs
+//   const tabEl = $(".tab-item");
+//   const tabTit = tabEl.find(".tit");
 
-  // tab-item 옆 .on 삭제
-  tabEl.removeClass("on");
-  // .tab-item:first-child에게 .on 추가
-  // $(".tab-item:first-child").addClass("on");
-  tabEl.first().addClass("on");
+//   // tab-item 옆 .on 삭제
+//   tabEl.removeClass("on");
+//   // .tab-item:first-child에게 .on 추가
+//   // $(".tab-item:first-child").addClass("on");
+//   tabEl.first().addClass("on");
 
-  // .tab-item .tit을 클릭했을 때
-  tabTit.on("click", function () {
-    // .tab-item .on 삭제
-    tabEl.removeClass("on");
-    // 방금 클릭한 나(this)의 부모(parent()에게 .on 추가
-    $(this).parents().addClass("on");
-  });
-});
+//   // .tab-item .tit을 클릭했을 때
+//   tabTit.on("click", function () {
+//     // .tab-item .on 삭제
+//     tabEl.removeClass("on");
+//     // 방금 클릭한 나(this)의 부모(parent()에게 .on 추가
+//     $(this).parents().addClass("on");
+//   });
+// });
+
+// // document.addEventListener("DOMContentLoaded", function () {
+// //   const tabTits = document.querySelectorAll(".tab-item .tit");
+// //   const depth2Menus = document.querySelectorAll(".depth2");
+
+// //   // tabTits.classList.add("on");
+// //   tabTits.forEach(function (tabTit) {
+// //     tabTit.addEventListener("click", function () {
+// //       // 모든 하위 메뉴 숨기기
+// //       depth2Menus.forEach(function (depth2) {
+// //         depth2.classList.remove("checked");
+// //       });
+// //       // 클릭한 탭의 하위 메뉴 표시
+// //       const parentTab = this.parentElement;
+// //       const depth2ToDisplay = parentTab.querySelector(".depth2");
+// //       if (depth2ToDisplay) {
+// //         depth2ToDisplay.classList.add("checked");
+// //       }
+// //     });
+// //   });
+// // });
